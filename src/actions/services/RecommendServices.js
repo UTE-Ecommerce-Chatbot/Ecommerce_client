d3ca8d02bf315d33e5faf30137910ac48199b678
Tien Nguyen Phuc <59163718+nguyenphuctien4865@users.noreply.github.com>
2024-07-17 09:51:37 +0700

    Fix Carousel

--- src/actions/services/RecommendServices.js
+++ src/actions/services/RecommendServices.js
@@ -1,22 +1,20 @@
-import {
-    API_URL
-} from '../constants/constants'
-import axios from 'axios'
-import axiosClient from 'actions/constants/axiosClient'
+import { API_URL } from "../constants/constants";
+import axios from "axios";
+import axiosClient from "actions/constants/axiosClient";
 
-export const getSimilarProduct = (tags, category) => {
-    // const tagsData = {tags: tags}
-    return axios({
-        method: 'POST',
-        data: tags,
-        url: `${API_URL}/api/recommend/similar/${category}`
-    })
-}
+export const getSimilarProduct = (tags, category, brand) => {
+  // const tagsData = {tags: tags}
+  return axios({
+    method: "POST",
+    data: tags,
+    url: `${API_URL}/api/recommend/similar/${category}`,
+  });
+};
 
 export const getListRecommendForUser = () => {
-    // const tagsData = {tags: tags}
-    return axiosClient({
-        method: 'POST',
-        url: `${API_URL}/api/recommend/list`,
-    })
-}
+  // const tagsData = {tags: tags}
+  return axiosClient({
+    method: "POST",
+    url: `${API_URL}/api/recommend/list`,
+  });
+};
--- src/components/Item/ProductItem.jsx
+++ src/components/Item/ProductItem.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { currency } from "../../utils/FormatCurrency";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 function ProductItem(props) {
   let { products } = props;
 
@@ -17,17 +19,21 @@ function ProductItem(props) {
         const price = currency(item.price);
         return (
           <div className="col l-2 m-2 c-6" key={item.id}>
-            <Link to={`/san-pham/${item.id}/${item.slug}`} className="home-product-item-link">
+            <Link
+              to={`/san-pham/${item.id}/${item.slug}`}
+              className="home-product-item-link"
+            >
               <div className="home-product-item">
                 <div
                   className="home-product-item__img"
-                  style={{ backgroundImage: `url(${item.mainImage})`,
-                }}
+                  style={{ backgroundImage: `url(${item.mainImage})` }}
                 />
                 <h4 className="home-product-item__name">{item.name}</h4>
                 <div className="home-product-item-price">
                   <span className="home-product-item-price-new">{price}</span>
-                  <span className="home-product-item-seller">Đã bán {item.seller_count}</span>
+                  <span className="home-product-item-seller">
+                    Đã bán {item.seller_count}
+                  </span>
                 </div>
                 <div className="home-product-item__info">
                   <span className="home-product-item__brand">
@@ -42,7 +48,9 @@ function ProductItem(props) {
                   <span>Yêu thích</span>
                 </div>
                 <div className="home-product-item__sales">
-                  <span className="home-product-item__sales-percent">{item.percent_discount}%</span>
+                  <span className="home-product-item__sales-percent">
+                    {item.percent_discount}%
+                  </span>
                   <span className="home-product-item__sales-label">GIẢM</span>
                 </div>
               </div>
@@ -55,3 +63,74 @@ function ProductItem(props) {
 }
 
 export default ProductItem;
+
+// import styles from "./ProductItem.module.css";
+
+// function ProductItem(props) {
+//   let { products } = props;
+
+//   if (products?.length === 0 || products === undefined) {
+//     products = [];
+//   }
+//   const productsArray = Array.from(products);
+
+//   return (
+//     <>
+//       <Carousel
+//         showArrows={true}
+//         infiniteLoop
+//         autoPlay={false}
+//         centerMode={true}
+//         itemsToShow={6}
+//         showStatus={false}
+//         showThumbs={false} // Hide thumbnails
+//       >
+//         {productsArray.map((item) => {
+//           const price = currency(item.price);
+//           return (
+//             <div className={styles.carouselItem} key={item.id}>
+//               <Link
+//                 to={`/san-pham/${item.id}/${item.slug}`}
+//                 className="home-product-item-link"
+//               >
+//                 <div className={styles.productItem}>
+//                   <div
+//                     className={styles.productItemImage}
+//                     style={{ backgroundImage: `url(${item.mainImage})` }}
+//                   />
+//                   <h4 className={styles.productItemName}>{item.name}</h4>
+//                   <div className={styles.productItemPrice}>
+//                     <span className="home-product-item-price-new">{price}</span>
+//                     <span className="home-product-item-seller">
+//                       Đã bán {item.seller_count}
+//                     </span>
+//                   </div>
+//                   <div className={styles.productItemInfo}>
+//                     <span className="home-product-item__brand">
+//                       {item.brand ? item.brand.name : ""}
+//                     </span>
+//                     <span className="home-product-item__brand-name">
+//                       {item.brand ? item.brand.madeIn : ""}
+//                     </span>
+//                   </div>
+//                   <div className={styles.productItemFavourite}>
+//                     <i className="fas fa-check" />
+//                     <span>Yêu thích</span>
+//                   </div>
+//                   <div className={styles.productItemSales}>
+//                     <span className="home-product-item__sales-percent">
+//                       {item.percent_discount}%
+//                     </span>
+//                     <span className="home-product-item__sales-label">GIẢM</span>
+//                   </div>
+//                 </div>
+//               </Link>
+//             </div>
+//           );
+//         })}
+//       </Carousel>
+//     </>
+//   );
+// }
+
+// export default ProductItem;
